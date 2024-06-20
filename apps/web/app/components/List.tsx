b34a69a9179389f9e5f672dd645bb27d3b3a6718
Peter Gál <pergalsk@gmail.com>
2024-06-20 23:53:12 +0200

    Added prettier with prettier-plugin-tailwindcss plugin

--- apps/web/app/components/List.tsx
+++ apps/web/app/components/List.tsx
@@ -8,7 +8,7 @@ interface ListProps {
 
 export function List({ data, selected, onSelect }: ListProps): JSX.Element {
   return (
-    <div className="flex flex-col flex-none lg:w-1/2 lg:h-full h-1/2 w-full p-2 bg-bunker-950 border-r border-thin border-bunker-700 gap-2 overflow-auto">
+    <div className="bg-bunker-950 border-thin border-bunker-700 flex h-1/2 w-full flex-none flex-col gap-2 overflow-auto border-r p-2 lg:h-full lg:w-1/2">
       {data.map((item, index) => (
         <ListItem
           key={index}

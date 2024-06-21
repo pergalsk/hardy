2dbf5902a71d7597a395fb2ec19baec1625c348d
Peter Gál <pergalsk@gmail.com>
2024-06-21 19:58:44 +0200

    Headers are table now

--- apps/web/app/components/Headers.tsx
+++ apps/web/app/components/Headers.tsx
@@ -4,16 +4,25 @@ export interface HeaderItem {
 }
 export function Headers({ headers }: { headers: HeaderItem[] }): JSX.Element {
   return (
-    <div className="flex flex-col gap-1 text-sm">
-      {Array.isArray(headers) &&
-        headers
-          .sort((a: HeaderItem, b: HeaderItem) => a.name.localeCompare(b.name))
-          .map((header: HeaderItem, index: number) => (
-            <div key={index} className="text-mirage-200 font-mono break-all">
-              <span className="font-bold pr-2">{header.name}:</span>
-              {header.value}
-            </div>
-          ))}
-    </div>
+    <table className="table-auto text-sm">
+      <tbody>
+        {Array.isArray(headers) &&
+          headers
+            .sort((a: HeaderItem, b: HeaderItem) =>
+              a.name.localeCompare(b.name),
+            )
+            .map((header: HeaderItem, index: number) => (
+              <tr
+                key={index}
+                className="text-mirage-200 border-bunker-400 break-all border-b font-mono"
+              >
+                <td className="w-auto whitespace-nowrap py-1 pr-2 align-top font-bold">
+                  {header.name}:
+                </td>
+                <td className="py-1">{header.value}</td>
+              </tr>
+            ))}
+      </tbody>
+    </table>
   );
 }

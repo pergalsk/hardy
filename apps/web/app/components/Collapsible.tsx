2352d7e457545f4feb60fbcdd5109a6d46f52569
Peter Gál <pergalsk@gmail.com>
2024-07-31 22:56:59 +0200

    Enhanced collapsible icon

--- apps/web/app/components/Collapsible.tsx
+++ apps/web/app/components/Collapsible.tsx
@@ -8,6 +8,7 @@ export function Collapsible({
   title: string;
 }) {
   const [isOpen, setIsOpen] = useState(true);
+  const rotation = isOpen ? "rotate-270" : "-rotate-90";
 
   return (
     <div className="border-mirage-400">
@@ -15,8 +16,12 @@ export function Collapsible({
         className="bg-mirage-50 text-mirage-700 dark:bg-bunker-700 dark:text-mirage-600 hover:bg-mirage-100 dark:hover:bg-bunker-600 mr-2 flex flex-1 cursor-pointer items-center justify-start gap-2 rounded-md p-3 py-1 transition-colors duration-200"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <div>{isOpen ? "▲" : "▼"}</div>
-        <div className="font-bold uppercase">{title}</div>
+        <div
+          className={`transform transition-transform duration-200 ${rotation}`}
+        >
+          ▼
+        </div>
+        <div className="select-none font-bold uppercase">{title}</div>
       </div>
       {isOpen && <div className="p-2 pb-0">{children}</div>}
     </div>

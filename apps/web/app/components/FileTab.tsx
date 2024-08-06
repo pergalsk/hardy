8d3c3e88b665941e4406b8b766626edba1d8dc6d
Peter Gál <pergalsk@gmail.com>
2024-08-06 22:39:53 +0200

    Restyled header with file tabs

--- apps/web/app/components/FileTab.tsx
+++ apps/web/app/components/FileTab.tsx
@@ -11,10 +11,10 @@ export function FileTab({ file }: FileTabProps): JSX.Element {
   const { name, fileId } = file;
 
   return (
-    <div className="dark:bg-bunker-900 dark:text-mirage-200 border-accent-100 dark:border-accent-700 mt-1 flex cursor-default select-none items-center justify-center rounded-t-xl border-t-2 bg-white px-4 py-0 pr-2 pt-2 text-sm font-bold text-black">
+    <div className="dark:bg-bunker-900 dark:text-mirage-200 border-accent-100 flex cursor-default select-none items-center justify-center rounded-lg bg-white px-4 py-0 pr-1 text-sm font-bold text-black">
       {name}
       <span
-        className="hover:bg-accent-50 dark:bg-bunker-600 dark:hover:bg-accent-600 dark:text-mirage-200 ml-2 cursor-pointer rounded bg-white px-2 py-1 font-bold text-black transition-colors duration-200 dark:hover:text-white"
+        className="hover:bg-accent-50 dark:bg-bunker-600 dark:hover:bg-accent-600 dark:text-mirage-200 ml-2 cursor-pointer rounded-md bg-white px-2 py-1 font-bold text-black transition-colors duration-200 dark:hover:text-white"
         onClick={() => removeFile(fileId)}
       >
         ⨉

41b0d3b8903d9117c6c9b648264b40f1a96629bb
Peter Gál <pergalsk@gmail.com>
2024-08-24 01:11:19 +0200

    Time component is now more null pointer secure

--- apps/web/app/components/Time.tsx
+++ apps/web/app/components/Time.tsx
@@ -1,3 +1,6 @@
-export function Time({ time }: { time: number }) {
+export function Time({ time }: { time: number }): JSX.Element | null {
+  if (typeof time !== "number") {
+    return null;
+  }
   return <span>{time.toFixed(2)}ms</span>;
 }

9fef0b4cec1fb37bdc978cae13fc3a697492674c
Peter Gál <pergalsk@gmail.com>
2024-08-27 22:47:01 +0200

    Added JsonViewer light theme (pt.1)

--- apps/web/app/components/JsonContent.tsx
+++ apps/web/app/components/JsonContent.tsx
@@ -2,7 +2,39 @@ import JsonView from "@uiw/react-json-view";
 import { useAppStore } from "../store/store";
 import { selectJsonViewerSettings } from "../store/selectors";
 
-const style: { [key: string]: string } = {
+const lightStyle: { [key: string]: string } = {
+  "--w-rjv-color": "#9cdcfe",
+  "--w-rjv-background-color": "#fff",
+  "--w-rjv-key-string": "#185d81",
+  "--w-rjv-edit-color": "#9cdcfe",
+  "--w-rjv-info-color": "#9c9c9c7a",
+  "--w-rjv-update-color": "#9cdcfe",
+  "--w-rjv-copied-color": "#9cdcfe",
+  "--w-rjv-copied-success-color": "#28a745",
+
+  "--w-rjv-key-number": "#185d8196",
+  "--w-rjv-curlybraces-color": "#185d8196",
+  "--w-rjv-colon-color": "#185d8196",
+  "--w-rjv-brackets-color": "#185d8196",
+  "--w-rjv-ellipsis-color": "#ff0000",
+  "--w-rjv-quotes-color": "#185d8196",
+  "--w-rjv-quotes-string-color": "#185d8196",
+  "--w-rjv-line-color": "#f1f5f9",
+  "--w-rjv-arrow-color": "#185d8196",
+
+  "--w-rjv-type-string-color": "#a68c00",
+  "--w-rjv-type-int-color": "#1cc212",
+  "--w-rjv-type-float-color": "#1cc212",
+  "--w-rjv-type-bigint-color": "#1cc212",
+  "--w-rjv-type-boolean-color": "#569cd6",
+  "--w-rjv-type-date-color": "#b5cea8",
+  "--w-rjv-type-url-color": "#3b89cf",
+  "--w-rjv-type-null-color": "#fa8072",
+  "--w-rjv-type-nan-color": "#859900",
+  "--w-rjv-type-undefined-color": "#fa8072",
+};
+
+const darkStyle: { [key: string]: string } = {
   "--w-rjv-color": "#9cdcfe",
   "--w-rjv-key-number": "#b3d9ed88",
   "--w-rjv-key-string": "#b3d9ed",
@@ -39,7 +71,7 @@ export function JsonContent({ data }: { data: any }): JSX.Element {
 
   return (
     <div className="break-all">
-      <JsonView value={data} style={style} {...settings} />
+      <JsonView value={data} style={darkStyle} {...settings} />
     </div>
   );
 }

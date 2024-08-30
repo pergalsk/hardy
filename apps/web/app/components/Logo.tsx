5ec7302bfaa4e7e78ac7f32875ac730b233745bb
Peter Gál <pergalsk@gmail.com>
2024-08-30 12:13:54 +0200

    Modified logo design

--- apps/web/app/components/Logo.tsx
+++ apps/web/app/components/Logo.tsx
@@ -1,10 +1,10 @@
 export function Logo(): JSX.Element {
   return (
-    <div className="flex select-none content-center gap-3 text-lg font-black tracking-wide">
+    <div className="flex select-none content-center gap-3 font-black tracking-wide">
       <div className="rounded-lg bg-yellow-400 px-3 py-1 text-black dark:bg-yellow-500">
-        HARDY
+        H
       </div>
-      <div className="flex items-center text-white">HAR viewer</div>
+      <div className="flex items-center text-white">HARDY</div>
     </div>
   );
 }

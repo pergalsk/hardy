5d03ec41e0dacc297872a3b4a094d2b2f19dd845
Peter Gál <pergalsk@gmail.com>
2024-08-30 14:58:17 +0200

    Improved buttons layout (paddings)

--- apps/web/app/components/ToggleMark.tsx
+++ apps/web/app/components/ToggleMark.tsx
@@ -1,8 +1,11 @@
 export function ToggleMark({ opened }: { opened: boolean }): JSX.Element {
-  const rotation = opened ? "rotate-270" : "-rotate-90";
+  const rotation = opened ? "rotate-90" : "rotate-0";
+
   return (
-    <div className={`transform transition-transform duration-200 ${rotation}`}>
-      ▼
+    <div className="my-auto flex align-middle">
+      <span
+        className={`${rotation} iconify material-symbols--play-arrow-outline-rounded relative bottom-[2px] transform text-xl transition-transform duration-200`}
+      ></span>
     </div>
   );
 }

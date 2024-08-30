6f5bfa71ad0f343c3ddaff3c469fde2d8fb6f39d
Peter Gál <pergalsk@gmail.com>
2024-08-30 15:06:38 +0200

    Improved UI of file tabs

--- apps/web/app/components/FileTab.tsx
+++ apps/web/app/components/FileTab.tsx
@@ -11,7 +11,7 @@ export function FileTab({ file }: FileTabProps): JSX.Element {
   const { name, fileId } = file;
 
   return (
-    <div className="border-accent-100 bg-accent-700 flex select-none items-center justify-center rounded-lg px-4 py-0 pr-1 text-sm font-bold text-white">
+    <div className="border-accent-100 bg-accent-700 flex select-none items-center justify-center rounded-md py-0 pl-3 pr-0.5 text-sm font-bold text-white">
       {name}
       <span
         className="hover:bg-accent-800 bg-accent-700 ml-2 rounded-md px-2 py-1 font-bold text-white transition-colors duration-200 dark:hover:text-white"

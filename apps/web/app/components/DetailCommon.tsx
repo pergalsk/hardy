52de6efb0cf1d5da3446592c90329f08fc809ac9
Peter Gál <pergalsk@gmail.com>
2024-09-01 00:20:13 +0200

    Added collapsible URL section

--- apps/web/app/components/DetailCommon.tsx
+++ apps/web/app/components/DetailCommon.tsx
@@ -6,9 +6,11 @@ import { Method } from "./Method";
 import { Status } from "./Status";
 import { Time } from "./Time";
 import { DetailField } from "./DetailField";
+import { useLineClamp } from "../helpers/useLineClamp";
 
 export function DetailCommon(): JSX.Element | null {
   const data = useAppStore(selectCommonData);
+  const [ref, isCollapsed] = useLineClamp();
 
   if (!data) {
     return null;
@@ -19,9 +21,16 @@ export function DetailCommon(): JSX.Element | null {
 
   return (
     <div className="bg-mirage-50 dark:bg-bunker-500 flex flex-1 flex-col gap-1 rounded-md p-2">
-      <DetailField label={"URL:"}>
-        <Url url={url} />
-      </DetailField>
+      <div className="relative line-clamp-3" ref={ref}>
+        <DetailField label={"URL:"}>
+          <Url url={url} />
+        </DetailField>
+        {isCollapsed ? (
+          <button className="dark:text-accent-300 dark:bg-bunker-500 hover:dark:text-accent-100 absolute bottom-0 right-0 inline select-none rounded-sm pl-2 uppercase">
+            ...More
+          </button>
+        ) : null}
+      </div>
 
       <hr className="border-mirage-100 dark:border-bunker-700 my-1 border border-b" />
 
---
+++ apps/web/app/helpers/useLineClamp.ts
@@ -0,0 +1,29 @@
+import { useCallback, useRef, useState } from "react";
+
+export function useLineClamp() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const previousObserver = useRef(null);
+
+  // todo: proper typing
+  const customRef: any = useCallback((node: HTMLElement) => {
+    if (previousObserver.current) {
+      (previousObserver.current as ResizeObserver).disconnect();
+      previousObserver.current = null;
+    }
+
+    if (node?.nodeType !== Node.ELEMENT_NODE) {
+      return;
+    }
+
+    const observer = new ResizeObserver(() => {
+      const collapsed = node.scrollHeight > node.clientHeight;
+      setIsCollapsed(collapsed);
+    });
+
+    observer.observe(node);
+    (previousObserver.current as unknown) = observer; // todo: proper typing
+  }, []);
+
+  return [customRef, isCollapsed];
+}

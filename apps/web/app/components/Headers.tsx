1fb78752cf855f22300286344df0438b833acfda
Peter Gál <pergalsk@gmail.com>
2024-09-06 21:23:04 +0200

    Attempt to User Agent formatter implementation (not fully working yet)

--- apps/web/app/components/Headers.tsx
+++ apps/web/app/components/Headers.tsx
@@ -1,8 +1,82 @@
+import { UAParser } from "ua-parser-js";
 import { LineClamp } from "./LineClamp";
+import { FormatterProvider } from "../providers/FormatterProvider";
 
 export interface HeaderItem {
   name: string;
-  value: string;
+  value: string | null | undefined;
+}
+
+export type HeaderValueFormatter = {
+  icon: string;
+  tooltip: string;
+  format: (headerItem: HeaderItem) => JSX.Element | string | null;
+};
+
+const headerValueFormatters = FormatterProvider<HeaderValueFormatter>();
+
+const userAgentParseFormatter: HeaderValueFormatter = {
+  icon: "iconify material-symbols--desktop-windows-outline-rounded",
+  tooltip: "Parse User Agent",
+  format: (headerItem: HeaderItem): string => {
+    const { value } = headerItem;
+    const { name: browser, version } = new UAParser(value ?? "").getBrowser();
+
+    return `${browser} ${version}`;
+  },
+};
+
+const userAgentBgFormatter: HeaderValueFormatter = {
+  icon: "iconify material-symbols--background-replace-rounded",
+  tooltip: "User Agent+",
+  format: (headerItem: HeaderItem): JSX.Element => {
+    const { value } = headerItem;
+    return <span className="bg-red-900">{value}</span>;
+  },
+};
+
+const id1 = headerValueFormatters.addFormatter(
+  "User-Agent",
+  userAgentParseFormatter,
+);
+const id2 = headerValueFormatters.addFormatter(
+  "User-Agent",
+  userAgentBgFormatter,
+);
+
+export function HeaderValue({
+  header,
+}: {
+  header: HeaderItem;
+}): JSX.Element | string {
+  const formatters = headerValueFormatters.getFormatters(header.name) || {};
+
+  const applyFormat = (
+    format: HeaderValueFormatter["format"],
+    tooltip: string,
+  ) => {
+    alert(tooltip);
+  };
+
+  const Icons = (): JSX.Element => (
+    <span className="my-auto ml-2 inline-flex items-center gap-1 align-middle dark:text-white">
+      {Object.entries(formatters).map(([, { tooltip, icon, format }]) => {
+        return (
+          <span
+            className={`dark:hover:text-accent-100 text-lg ${icon}`}
+            onClick={() => applyFormat(format, tooltip)}
+          ></span>
+        );
+      })}
+    </span>
+  );
+
+  return (
+    <>
+      {header.value || ""}
+      <Icons />
+    </>
+  );
 }
 
 export function Headers({ headers }: { headers: HeaderItem[] }): JSX.Element {
@@ -25,10 +99,10 @@ export function Headers({ headers }: { headers: HeaderItem[] }): JSX.Element {
                 <td className="py-1">
                   <LineClamp
                     lines={2}
-                    active={header.value.length > 100}
+                    active={(header.value || "").length > 100}
                     classes={"dark:bg-bunker-900 bg-white"}
                   >
-                    {header.value}
+                    <HeaderValue header={header} />
                   </LineClamp>
                 </td>
               </tr>

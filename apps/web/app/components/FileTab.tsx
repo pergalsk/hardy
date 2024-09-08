401a64fb19f406961490d99c0053156070591705
Peter Gál <pergalsk@gmail.com>
2024-09-08 20:13:01 +0200

    Added icon to file tabs

--- apps/web/app/components/FileTab.tsx
+++ apps/web/app/components/FileTab.tsx
@@ -11,10 +11,11 @@ export function FileTab({ file }: FileTabProps): JSX.Element {
   const { name, fileId } = file;
 
   return (
-    <div className="border-accent-100 bg-accent-700 flex select-none items-center justify-center rounded-md py-0 pl-3 pr-0.5 text-sm font-bold text-white">
-      <span className="relative top-[1px]">{name}</span>
+    <div className="border-accent-100 bg-accent-700 flex select-none items-center justify-center gap-1 rounded-md py-0 pl-2 pr-0.5 text-sm font-bold text-white">
+      <span className="iconify material-symbols--lab-profile-outline-rounded relative bottom-[0.02em] text-lg"></span>
+      <span>{name}</span>
       <span
-        className="hover:bg-accent-800 bg-accent-700 ml-2 flex rounded-md p-1 align-middle text-white transition-colors duration-200 dark:hover:text-white"
+        className="hover:bg-accent-800 bg-accent-700 flex rounded-md p-1 align-middle text-white transition-colors duration-200 dark:hover:text-white"
         onClick={() => removeFile(fileId)}
       >
         <span className="iconify material-symbols--close-rounded my-auto text-[1rem]"></span>

1cd00a406c3b3407bb736d147752da3b802af05a
Peter Gál <pergalsk@gmail.com>
2024-09-19 22:02:43 +0200

    Clear the filter when closing a file

--- apps/web/app/components/FileTab.tsx
+++ apps/web/app/components/FileTab.tsx
@@ -1,4 +1,4 @@
-import { removeFile } from "../store/actions";
+import { clearFilter, removeFile } from "../store/actions";
 
 interface FileTabProps {
   file: {
@@ -10,13 +10,18 @@ interface FileTabProps {
 export function FileTab({ file }: FileTabProps): JSX.Element {
   const { name, fileId } = file;
 
+  const handleFileClose = () => {
+    removeFile(fileId);
+    clearFilter();
+  };
+
   return (
     <div className="border-accent-100 bg-accent-700 flex select-none items-center justify-center gap-1 rounded-md py-0 pl-2 pr-0.5 text-sm font-bold text-white">
       <span className="iconify material-symbols--lab-profile-outline-rounded relative bottom-[0.02em] text-lg"></span>
       <span>{name}</span>
       <span
         className="hover:bg-accent-800 bg-accent-700 flex rounded-md p-1 align-middle text-white transition-colors duration-200 dark:hover:text-white"
-        onClick={() => removeFile(fileId)}
+        onClick={() => handleFileClose()}
       >
         <span className="iconify material-symbols--close-rounded my-auto text-[1rem]"></span>
       </span>

426bd0819dbf811a7f5a0b0c5ce8675eb2c6cb11
Peter Gál <pergalsk@gmail.com>
2024-09-19 22:40:48 +0200

    Cookies hover colors

--- apps/web/app/components/TrueFalseMark.tsx
+++ apps/web/app/components/TrueFalseMark.tsx
@@ -8,7 +8,7 @@ export function TrueFalseMark({
       Y
     </span>
   ) : value === false ? (
-    <span className="dark:bg-bunker-600 rounded-md bg-slate-100 px-2 py-1 group-hover:bg-slate-200">
+    <span className="dark:bg-bunker-600 dark:group-hover:bg-bunker-200 rounded-md bg-slate-100 px-2 py-1 group-hover:bg-slate-200">
       N
     </span>
   ) : null;

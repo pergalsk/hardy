6b9e289827827205fdc6aea5f8c8c120340e358f
Peter Gál <pergalsk@gmail.com>
2024-09-20 22:53:51 +0200

    Refactored drag and drop file (moved to separate hook)

--- apps/web/app/components/FileDropper.tsx
+++ apps/web/app/components/FileDropper.tsx
@@ -1,5 +1,11 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  LegacyRef,
+} from "react";
 import { useDarkMode } from "../helpers/useDarkMode";
 import { readFileData } from "../helpers/helpers";
 import { nanoid } from "../helpers/nanoid";
@@ -8,78 +14,93 @@ import { FileOpener } from "./FileOpener";
 import { FileOpenLightSvg } from "./FileOpenLightSvg";
 import { FileOpenDarkSvg } from "./FileOpenDarkSvg";
 
-export const FileDropper = () => {
+export function useDragging<T extends HTMLElement>(
+  callbackFn: (e: DragEvent) => void,
+) {
+  const ref = useRef<T>(null);
   const [isDragging, setIsDragging] = useState(false);
-  const isDark = useDarkMode();
 
-  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+  const onDragEnter = useCallback((e: DragEvent) => {
     e.preventDefault();
     setIsDragging(true);
-  };
+  }, []);
 
-  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+  const onDragleave = useCallback((e: DragEvent) => {
     e.preventDefault();
     setIsDragging(false);
-  };
+  }, []);
 
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+  const onDragover = useCallback((e: DragEvent) => {
     e.preventDefault();
-  };
+  }, []);
 
-  const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+  const onDrop = useCallback((e: DragEvent) => {
     e.preventDefault();
     setIsDragging(false);
-
-    const file = e.dataTransfer.files[0];
-
-    if (!file) {
-      return;
-    }
-
-    const { name, size } = file;
-    const fileId = nanoid();
-
-    try {
-      const rawData = await readFileData(file);
-      const data = JSON.parse(rawData);
-      addFile({ fileId, name, size, data });
-      setRowId(0);
-    } catch (error) {
-      console.error("Error loading file:", error);
-      addToast({
-        type: "alert",
-        message: (
-          <>
-            File{" "}
-            <span className="underline-offset-3 italic underline">{name}</span>{" "}
-            cannot be opened. Wrong or disrupted content.
-          </>
-        ),
-      });
-    }
-  };
+    callbackFn(e);
+  }, []);
 
   useEffect(() => {
-    const handleDragOverPreventDefault = (e: DragEvent) => {
-      e.preventDefault();
-    };
-
-    window.addEventListener("dragover", handleDragOverPreventDefault);
-    window.addEventListener("drop", handleDragOverPreventDefault);
+    if (ref.current != null) {
+      ref.current.addEventListener("dragenter", onDragEnter);
+      ref.current.addEventListener("dragleave", onDragleave);
+      ref.current.addEventListener("dragover", onDragover);
+      ref.current.addEventListener("drop", onDrop);
+    }
 
     return () => {
-      window.removeEventListener("dragover", handleDragOverPreventDefault);
-      window.removeEventListener("drop", handleDragOverPreventDefault);
+      if (ref.current != null) {
+        ref.current.removeEventListener("dragenter", onDragEnter);
+        ref.current.removeEventListener("dragleave", onDragleave);
+        ref.current.removeEventListener("dragover", onDragover);
+        ref.current.removeEventListener("drop", onDrop);
+      }
     };
-  }, []);
+  }, [ref.current]);
+
+  return [ref, isDragging];
+}
+
+async function openFile(file: File) {
+  const { name, size } = file;
+  const fileId = nanoid();
+
+  try {
+    const rawData = await readFileData(file);
+    const data = JSON.parse(rawData);
+    addFile({ fileId, name, size, data });
+    setRowId(0);
+  } catch (error) {
+    console.error("Error loading file:", error);
+    addToast({
+      type: "alert",
+      message: (
+        <>
+          File{" "}
+          <span className="underline-offset-3 italic underline">{name}</span>{" "}
+          cannot be opened. Wrong or disrupted content.
+        </>
+      ),
+    });
+  }
+}
+
+function processDraggedFile(e: DragEvent) {
+  const file = e.dataTransfer?.files[0];
+  if (!file) {
+    return;
+  }
+  openFile(file);
+}
+
+export const FileDropper = () => {
+  const isDark = useDarkMode();
+  const [ref, isDragging] = useDragging<HTMLDivElement>(processDraggedFile);
 
   return (
     <div
       className={`${isDragging ? "border-accent-600 text-accent-600" : "border-mirage-200 dark:border-bunker-100 dark:text-mirage-400 text-mirage-500"} flex h-full w-full select-none flex-row items-center justify-center gap-3 rounded-2xl border-4 border-dashed font-bold portrait:flex-col`}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
+      ref={ref as LegacyRef<HTMLDivElement>}
     >
       <div className="flex h-1/2 max-h-[800px] w-2/3 max-w-[800px] md:h-2/3 md:w-2/5">
         {isDark ? <FileOpenDarkSvg /> : <FileOpenLightSvg />}

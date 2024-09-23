096a0962d3d459dda4f9cca2a399253c740263d7
Peter Gál <pergalsk@gmail.com>
2024-09-23 23:05:32 +0200

    Time with space

--- apps/web/app/components/Time.tsx
+++ apps/web/app/components/Time.tsx
@@ -2,5 +2,5 @@ export function Time({ time }: { time: number }): JSX.Element | null {
   if (typeof time !== "number") {
     return null;
   }
-  return <span>{time.toFixed(2)}ms</span>;
+  return <span>{time.toFixed(2)} ms</span>;
 }

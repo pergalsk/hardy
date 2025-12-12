631412329028b7ac70f681715fa7460a8b763e2d
Peter Gál <pergalsk@gmail.com>
2025-12-12 23:12:22 +0100

    Changed: hidden counts are bigger

--- apps/web/app/components/HiddenCount.tsx
+++ apps/web/app/components/HiddenCount.tsx
@@ -1,6 +1,6 @@
 export function HiddenCount({ count }: { count: number }): JSX.Element {
   return (
-    <div className="dark:text-accent-500 dark:after:border-accent-500 after:border-mirage-600 text-mirage-600 relative select-none text-center text-xs uppercase opacity-45 after:relative after:bottom-2 after:flex after:border-b after:border-dashed">
+    <div className="dark:text-accent-500 dark:after:border-accent-500 after:border-mirage-600 text-mirage-600 relative select-none text-center text-[0.7rem] uppercase opacity-45 after:relative after:bottom-2 after:flex after:border-b after:border-dashed">
       <span className="dark:bg-bunker-950 relative z-10 bg-white px-2">
         Hidden: {count}
       </span>

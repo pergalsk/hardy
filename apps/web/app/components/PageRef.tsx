84a2bcb3e526158665351499c575f8d9d47f3db0
Peter Gál <pergalsk@gmail.com>
2025-12-14 00:44:23 +0100

    Added pageref title as url

--- apps/web/app/components/PageRef.tsx
+++ apps/web/app/components/PageRef.tsx
@@ -1,10 +1,23 @@
+import { selectHarData } from "../store/selectors";
+import { useAppStore } from "../store/store";
+import { Url } from "./Url";
+
 export function PageRef({ pageref }: { pageref: any }) {
+  const harData = useAppStore(selectHarData);
+  const pages = harData?.pages || [];
+  const title = pages.find((page: any) => page.id === pageref)?.title || null;
+
   return (
     <div className="bg-bunker-950">
       <div
-        className={`${pageref ? "bg-bunker-500" : "bg-bunker-600"} mb-2 rounded-md p-2 text-sm uppercase text-white`}
+        className={`${pageref ? "bg-bunker-500" : "bg-bunker-600"} mb-2 flex gap-8 rounded-md p-2 text-sm text-white`}
       >
-        {pageref ?? "-"}
+        <div className="uppercase">{pageref ?? "-"}</div>
+        {title && (
+          <div className="ml-auto overflow-hidden text-ellipsis whitespace-nowrap text-right">
+            <Url url={title} />
+          </div>
+        )}
       </div>
     </div>
   );

4fa7afdf959a81c78e6b6e85b55ac214ac5f5537
Peter Gál <pergalsk@gmail.com>
2026-02-02 22:39:00 +0100

    Action icon - added disabled param

--- apps/web/app/components/ActionIcon.tsx
+++ apps/web/app/components/ActionIcon.tsx
@@ -3,10 +3,12 @@ import React from "react";
 export function ActionIcon({
   onClick,
   active,
+  disabled,
   icon,
 }: {
   onClick?: () => void;
   active?: boolean;
+  disabled?: boolean;
   icon?: string;
 }) {
   const base = "hover:text-accent-600 my-auto text-xl";
@@ -15,14 +17,16 @@ export function ActionIcon({
     ? "dark:text-accent-500 dark:hover:text-accent-300"
     : "dark:bg-slate-400 dark:hover:bg-slate-300";
 
+  const disabledClasses = disabled ? "opacity-50 pointer-events-none" : "";
+
   const handleClick = () => onClick?.();
 
   return (
     <div
       role="button"
       tabIndex={0}
-      onClick={handleClick}
-      className={`${stateClasses} ${base} ${icon ?? ""}`}
+      onClick={disabled ? undefined : handleClick}
+      className={`${stateClasses} ${base} ${disabledClasses} ${icon ?? ""}`}
     />
   );
 }

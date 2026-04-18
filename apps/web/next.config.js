a5a8051328191687d996049913936730ad2e9f67
Peter Gál <pergalsk@gmail.com>
2026-04-18 19:52:46 +0200

    Scaffold @repo/formatter-ui package and wire into host

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@repo/ui", "@repo/formatter-core"],
+  transpilePackages: ["@repo/ui", "@repo/formatter-core", "@repo/formatter-ui"],
   output: "export",
 };
